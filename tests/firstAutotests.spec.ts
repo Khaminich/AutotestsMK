81e65ad74d1ee9af0a6a9a80fc217fa18f741fd5
Nikita Haminich <n.haminich@pnn.in.ua>
2025-04-06 19:37:20 +0200

    Fixes

--- tests/firstAutotests.spec.ts
+++ tests/firstAutotests.spec.ts
@@ -41,6 +41,7 @@ test('Verify user can view product details', async ({ page }) => {
 test('Verify user can add product to cart', async ({ page }) => {
   await page.goto('https://practicesoftwaretesting.com');
 
+  //Assert1
   await page.getByText('Slip Joint Pliers').click()
   await expect(page.url()).toContain('https://practicesoftwaretesting.com/product');
 
@@ -49,5 +50,33 @@ test('Verify user can add product to cart', async ({ page }) => {
 
   const ProductPrice = page.locator('[data-test="unit-price"]');
   await expect(ProductPrice).toContainText("9.17");
+
+
+
+  //Assert2
+  await page.locator('[data-test="add-to-cart"]').click()
+
+  const alert = page.getByRole('alert', { name: 'Product added to shopping' });
+  await expect(alert).toBeVisible();
+  await expect(alert).toContainText("Product added to shopping cart");
+  await expect(alert).toBeHidden({ timeout: 8000 });
+
+  const CartIcon = page.locator('[data-test="nav-cart"]');
+  await expect (CartIcon).toContainText('1');
+
+
+  //Assert3
+  await page.locator('[data-test="nav-cart"]').click();
+  await expect(page).toHaveURL('https://practicesoftwaretesting.com/checkout');
+
+  const ProductQuantity = page.locator('[data-test="product-quantity"]');
+  await expect(ProductQuantity).toHaveCount(1);
+  
+
+  const ProductTitle = page.locator('.col-md-4');
+  await expect(ProductTitle).toContainText('Slip Joint Pliers');
+
+  const ProceedButton = page.locator('[data-test="proceed-1"]');
+  await expect(ProceedButton).toBeVisible();
 
 });
